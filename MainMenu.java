ed81c1e501d8681b19aceb4de2eab0e0d28af8cc
C Nicole Nwakalor <nicole.nwakalor@gmail.com>
2019-01-12 14:57:47 -0500

    Main Menu

---
+++ MainMenu.java
@@ -0,0 +1,13 @@
+package com.nicole.taskmanager;
+
+public class TaskManager {
+	// Main Menu Method
+		public void mainMenu() {
+			System.out.println("1. Add a task");
+			System.out.println("2. Remove a task");
+			System.out.println("3. Mark a task complete");
+			System.out.println("4. List the tasks");
+			System.out.println("    ");
+			System.out.println("What would you like to do?");
+		}	
+}
